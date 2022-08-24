dda996235bf0485facc62aadd32d7348afad4f32
iiotakamasa <tiio@mail.doshisha.ac.jp>
2022-08-24 22:56:59 +0900

    Refactored.

--- speechrec/README.md
+++ speechrec/README.md
@@ -11,7 +11,8 @@ PC上でコンテナを起動してから、Sota上でUDPの送信を開始す�
 
 ## PC
 ```
-docker run --name speechrec --mount type=bind,source="$(pwd)"/src,target=/tmp --net amqnet --rm speechrec python main.py --api_key <api_key> [--amq_host <amq_host> --amq_port <amq_port>]
+cd speechrec
+docker run --rm -it --name speechrec -p 5001:5001/udp --mount type=bind,source="$(pwd)"/src,target=/tmp speechrec python app.py --api_key <api_key>
 ```
 
 ## Sota
@@ -21,7 +22,9 @@ cd ffmpeg
 ./ffmpeg -channels 1 -f alsa -thread_queue_size 8192 -i hw:2 -preset ultrafast -tune zerolatency -ac 1 -c:a pcm_s16le -ar 16000 -f s16le udp://[host's ip]:5001?pkt_size=1024
 ```
 
+Sotaを使わずWindows PCのFFmpegを使う場合
+```
+ffmpeg -f dshow -ac 1 -thread_queue_size 8192 -i audio="マイク配列 (Realtek High Definition Audio(SST))" -f s16le -c:a pcm_s16le -ar 16000 udp://127.0.0.1:5001?pkt_size=1024
+```
+※マイク配列～の部分に入れる文字列は`ffmpeg -list_devices true -f dshow -i dummy`で調べられる。
 
-# 備考
-ffmpeg-pythonを用いて、受信したudpの音声パケットを標準出力にパイプで流し、そのパイプに入ってきたデータをAzureの音声認識APIに流している。
-そのため、printを書いても標準出力に表示されない（プログラムの終了後にまとめて表示される）
---
+++ speechrec/src/app.py
@@ -0,0 +1,83 @@
+import argparse
+import time
+import azure.cognitiveservices.speech as speechsdk
+import ffmpeg
+from pymongo import MongoClient
+
+
+def main(args):
+
+    # MongoDB
+    client = MongoClient(args.db_host, args.db_port)
+    db = client[args.db_name]
+    collection = db[args.db_collection_name]
+
+    # Config a speech recognizer
+    speech_config = speechsdk.SpeechConfig(subscription=args.api_key, region=args.region)
+    speech_config.speech_recognition_language=args.lang
+    stream = speechsdk.audio.PushAudioInputStream()
+    audio_config = speechsdk.audio.AudioConfig(stream=stream)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    # Callbacks for speech recog
+    def recognizing_cb(event):
+        """ 認識中の結果をデータベースに挿入 """
+        data = dict(timestamp=time.time(), state='recognizing', result=event.result.text)
+        print(data)
+        # collection.insert_one(data)
+
+    def recognized_cb(event):
+        """ 認識完了の結果をデータベースに挿入 """
+        data = dict(timestamp=time.time(), state='recognized', result=event.result.text)
+        print(data)
+        # collection.insert_one(data)
+
+    def session_started_cb(event):
+        pass
+
+    def session_stopped_cb(event):
+        pass
+
+    def canceled_cb(event):
+        pass
+    
+    # Connect callbacks to the events fired by the speech recognizer
+    speech_recognizer.recognizing.connect(recognizing_cb)
+    speech_recognizer.recognized.connect(recognized_cb)
+    speech_recognizer.session_started.connect(session_started_cb)
+    speech_recognizer.session_stopped.connect(session_stopped_cb)
+    speech_recognizer.canceled.connect(canceled_cb)
+
+    # FFmpeg
+    process = (
+        ffmpeg
+        .input(args.src, format='s16le', acodec='pcm_s16le', ac=1, ar='16k')
+        .output('-', format='s16le', acodec='pcm_s16le', ac=1, ar='16k')
+        .run_async(pipe_stdout=True)
+    )
+
+    # Start speech recognition
+    speech_recognizer.start_continuous_recognition()
+    try:
+        while True:
+            in_bytes = process.stdout.read(1024)
+            if not in_bytes:
+                break
+            stream.write(in_bytes)
+    except KeyboardInterrupt:
+        pass
+    speech_recognizer.stop_continuous_recognition()
+
+
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--api_key', required=True, type=str)
+    parse.add_argument('--region', default='japanwest', type=str)
+    parse.add_argument('--lang', default='ja-JP', type=str)
+    parse.add_argument('--src', default='udp://127.0.0.1:5001', type=str)
+    parse.add_argument('--db_host', default='mongo', type=str)
+    parse.add_argument('--db_port', default=27017, type=int)
+    parse.add_argument('--db_name', default='robot_talking_to_people', type=str)
+    parse.add_argument('--db_collection_name', default='speech_recognition', type=str)
+    args = parse.parse_args()
+    main(args)
