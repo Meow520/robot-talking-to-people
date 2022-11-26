6165a0dc0c266658011df1a3ef550c2b0529ff81
Meow520 <maomao.xmdhkdltm@google.com>
2022-11-26 19:16:18 +0900

    fix state

--- state/src/states.py
+++ state/src/states.py
@@ -29,9 +29,10 @@ class Idle(State):
     """ アイドル状態 """
     def transition(self) -> State:
         if self.person_appeared():
-            d = self.db['human_recognition'].find_one(sort=[( '_id', pymongo.DESCENDING)])
-            target_id = d['results'][0]['id']
-            return Greet(self.db, target_id)
+            d = self.db['human_recognition'].find_one(sort=[( 'timestamp', pymongo.DESCENDING)])
+            if len(d['results']) > 0:
+                target_id = d['results'][0]['id']
+                return Greet(self.db, target_id)
         return self
 
     def person_appeared(self) -> bool:
@@ -59,6 +60,9 @@ class Greet(State):
     def transition(self) -> State:
         if self.person_disappeared():
             return Idle(self.db)
+
+        if self.ask_to_people():
+            return Ask(self.db, self.target_id)
         return self
 
     def person_disappeared(self) -> bool:
@@ -70,4 +74,146 @@ class Greet(State):
             'results.0.id': self.target_id
         })
         return n == 0
+
+    def ask_to_people(self) -> bool:
+        d = self.db['state'].find_one(sort=[( 'timestamp', pymongo.DESCENDING)])
+        state_changetime = d['timestamp']
+
+        dt = 5
+        t = time.time()
+        m = self.db['human_recognition'].count_documents({
+            'timestamp': { '$gt': t - dt, '$lt': t }
+        })
+        n = self.db['human_recognition'].count_documents({
+            'timestamp': { '$gt': t - dt, '$lt': t },
+            'results' : { '$ne': [] }
+        })
+        if t - dt >= state_changetime:
+            return n >= m
+        return False
+
+class Ask(State):
+    def __init__(self, db, target_id:str):
+        super().__init__(db)
+        self.target_id = target_id
+
+    def to_document(self) -> dict:
+        return dict(name=self.__class__.__name__, target_id=self.target_id)
+
+    def transition(self) -> State:
+        if self.talk_to_people():
+            return Talk(self.db, self.target_id)
+        if self.bye_to_you():
+            return Bye(self.db, self.target_id)
+        if self.person_disappeared():
+            return Idle(self.db)
+        return self
+    
+
+    def person_disappeared(self) -> bool:
+        dt = 10
+        t = time.time()
+        n = self.db['human_recognition'].count_documents({
+            'timestamp':{'$gt': t - dt, '$lt': t},
+            'results.0.id': self.target_id
+        })
+        return n == 0
+
+    
+    def talk_to_people(self) -> bool:
+        d = self.db['state'].find_one(sort=[( 'timestamp', pymongo.DESCENDING)])
+        state_changetime = d['timestamp']
+
+        dt = 5
+        m = 1
+        t = time.time()
+        n = self.db['speech_recognition'].count_documents({
+            'timestamp': { '$gt': t - dt, '$lt': t },
+            'state': {'$eq': "recognized"},
+            'result' : { '$in': ["はい。"]}
+        })
+        
+        if t - dt >= state_changetime:
+            return n >= m
+        
+        return  False
+
+    def bye_to_you(self) -> bool:
+        d = self.db['state'].find_one(sort=[( 'timestamp', pymongo.DESCENDING)])
+        state_changetime = d['timestamp']
+        dt = 5
+        m = 1
+        t = time.time()
+        n = self.db['speech_recognition'].count_documents({
+            'timestamp': { '$gt': t - dt, '$lt': t },
+            'state': {'$eq': "recognized"},
+            'result' : { '$in': ["いいえ。"]}
+        })
+        if t - dt >= state_changetime:
+            return n >= m
+
+
+class Talk(State):
+    def __init__(self, db, target_id:str):
+        super().__init__(db)
+        self.target_id = target_id
+
+    def to_document(self) -> dict:
+        return dict(name=self.__class__.__name__, target_id=self.target_id)
+
+    def transition(self) -> State:
+        if self.person_disappeared():
+            return Idle(self.db)
+        if self.goodbye():
+            return Bye(self.db, self.target_id)
+
+        return self
+
+    def person_disappeared(self) -> bool:
+        dt = 10
+        t = time.time()
+        n = self.db['human_recognition'].count_documents({
+            'timestamp':{'$gt': t - dt, '$lt': t},
+            'results.0.id': self.target_id
+        })
+        return n == 0
+
+    def goodbye(self) -> bool:
+        d = self.db['state'].find_one(sort=[( 'timestamp', pymongo.DESCENDING)])
+        state_changetime = d['timestamp']
+        dt = 5
+        t = time.time()
+        n = self.db['human_recognition'].count_documents({
+            'timestamp':{'$gt': t - dt, '$lt': t},
+            'results.0.id': self.target_id
+        })
+        if t - dt >= state_changetime:
+            return n == 0
+
+class Bye(State):
+    def __init__(self, db, target_id:str):
+        super().__init__(db)
+        self.target_id = target_id
+    
+    def to_document(self) -> dict:
+        return dict(name=self.__class__.__name__, target_id=self.target_id)
+
+    def transition(self):
+        if self.person_disappeared():
+            return Idle(self.db)
+        return self
+
+    def person_disappeared(self):
+        dt = 10
+        t = time.time()
+        n = self.db['human_recognition'].count_documents({
+            'timestamp':{'$gt': t - dt, '$lt': t},
+            'results.0.id': self.target_id
+        })
+        return n == 0
+        
+
+
+
+
 
